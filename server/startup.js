fa9e3cea82f1103aaefdab3cc11239400f217b29
Leo Bergnéhr <leo@bergnehr.se>
2014-11-05 22:39:35 +0100

    Remove hard coded repo store root

--- server/startup.js
+++ server/startup.js
@@ -1,11 +1,17 @@
-RepoStoreRootPath = "/tmp/repos";
+var fs = Npm.require("fs");
 
 Meteor.startup(function() {
   var hg = Meteor.npmRequire("hg");
   var repo = new hg.HGRepo();
 
+  console.log(JSON.stringify(Meteor.settings));
+  var repoStoreRootPath = Meteor.settings.repoStoreRootPath;
+  if (!repoStoreRootPath) {
+    throw new Error("`repoStoreRootPath` not set. Configure in settings or in environment variable and restart server.");
+  }
+
   Meteor.setInterval(function() {
-    getRepositories(RepoStoreRootPath).forEach(function(repositoryPath) {
+    getRepositories(repoStoreRootPath).forEach(function(repositoryPath) {
       repo.pull(repositoryPath, {
         "-R": repositoryPath
       }, function(error) {
@@ -19,7 +25,6 @@ Meteor.startup(function() {
 });
 
 var getRepositories = function(rootPath) {
-  var fs = Npm.require("fs");
   var path = Npm.require("path");
 
   var repos = _.chain(fs.readdirSync(rootPath))
