34cde1e9481ed9b007c4e0e09912f38800e2cceb
niklas <noresjo@gmail.com>
2015-02-13 22:40:46 +0100

    Show search string if applicable in changeset view

--- client/routes.js
+++ client/routes.js
@@ -13,20 +13,21 @@ Router.route("/log/:repoName/:searchString(.*)?", {
 
   waitOn: function() {
     var params = this.params;
-
     return Meteor.subscribe("changesets", params.repoName, params.searchString);
   },
 
   data: function() {
     if (this.ready()) {
       var reponame = this.params.repoName;
+      var searchString = this.params.searchString;
       return {
         changesets: Changesets.find({}, {
           sort: [
             ["revision", "desc"]
           ]
         }),
-        repoName: reponame
+        repoName: reponame,
+        searchString: searchString
       };
     }
   }
